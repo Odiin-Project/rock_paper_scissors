d8734ef9fa7e3eee13c267eb3ff3b60a14692eae
AnnabelleEllis <44004057+AnnabelleEllis@users.noreply.github.com>
2022-12-27 02:21:36 -0500

    Added Opponent logic and got input from user (#2)
    * if comonient and user ties in the game - rock
    
    * add logic to determine if comonient or user won - rock
    
    * add logic to determine if comonient or user won - input taken from the user

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+function playRound (computerSelection, playerSelection) {
+    console.log(`You: ${playerSelection}, Computer: ${computerSelection}`);
+    if(computerSelection === playerSelection){
+        return `It's a TIE! You both played ${playerSelection}`;
+    } else if( computerSelection === "rock" && playerSelection === "paper"){
+        return "You Win! Paper beats Rock";
+    } else if( computerSelection === "rock" && playerSelection === "scissors"){
+        return "You Lose! Rock beats Scissors";
+    } else if( computerSelection === "paper" && playerSelection === "scissors"){
+        return "You Win! Scissors beats Paper";
+    } else if( computerSelection === "paper" && playerSelection === "rock"){
+        return "You Lose! Paper beats Rock";
+    } else if( computerSelection === "scissors" && playerSelection === "rock"){
+        return "You Win! Rock beats Scissors";
+    } else if( computerSelection === "scissors" && playerSelection === "paper"){
+        return "You Lose! Scissors beats Paper";
+    }
+}
+
+
 function getComputerChoice() {
     let arr = ["rock","paper","scissors"];
-    // console.log(arr[Math.round(Math.random() * 2)]);
     return arr[Math.round(Math.random() * 2)];
 }
 
-getComputerChoice();
-// console.log(getComputerChoice());
+const playerSelection = prompt("What's your move? - rock, paper or scissors");
+const computerSelection = getComputerChoice();
+console.log(`YouB: ${playerSelection}, ComputerB: ${computerSelection}`);
+
+console.log(playRound(computerSelection, playerSelection));
